a740c5809ec76c23159d2f3ce2b6196dd189d64a
Wesley Oliveira <WesleyOliveira.dev@outlook.com>
2021-09-24 20:51:32 -0300

    atividade1

---
+++ atividade-1/wesley.js
@@ -0,0 +1,15 @@
+const nome = 'Wesley'
+
+const sobrenome = 'Oliveira Alves'
+
+const ano = 2001
+
+const anoAtual = 2021
+
+const valor = anoAtual - ano
+
+const redes = ['facebook','instagram','linkedin','github','youtube','telegram']
+
+
+console.log(`${nome} ${sobrenome} faz ${valor} anos no ano ${anoAtual}`)
+
