a9a6fb5d40a2030b7cd40098b452998d529605ee
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-09-24 21:29:01 -0300

    Adicionado atividade 1

---
+++ atividade-1/marcus.js
@@ -0,0 +1,12 @@
+const primeiroNome = "Marcus";
+const anoNascimento = 1996;
+const anoAtual = 2021;
+const idadeAtual = anoAtual - anoNascimento
+const sobrenome = "Vinicius"
+
+//para exibir no console 
+console.log(`${primeiroNome} ${sobrenome} faz 
+${idadeAtual} anos no ano de ${anoAtual}`)
+
+const redesSociais = ["facebook", "youtube",
+"instagram"];
