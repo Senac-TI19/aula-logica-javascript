ea75561c98a4b8680f8569b88d0cb6546ea36057
natiherrera <natiquipildor@gmail.com>
2021-09-27 20:14:32 -0300

    aula logica

---
+++ atividade-1/natalia.js
@@ -0,0 +1,9 @@
+const nome = "Natalia"  
+const anoNascimento = 1994
+const anoAtual = 2021
+const idadeAtual = anoAtual - anoNascimento
+const sobrenome = "Herrera"
+
+console.log(`${nome} ${sobrenome} faz ${idadeAtual} anos no ano de ${anoAtual}`)
+
+const redesSociais = ["instagram", "youtube", "whatsapp", "twitter"]
