4887b51944f022b73c556c118a222daa86d4bb68
Jefferson Martins <jeffersonmandrade@gmail.com>
2021-09-27 20:37:02 -0300

    inclusão de novos passos na atividade 1

--- atividade-1/jefferson.js
+++ atividade-1/jefferson.js
@@ -1,2 +1,9 @@
 const nome = "Jefferson"
 const anoNascimento = 1988
+const anoAtual = 2021
+const idadeAtual = anoAtual - anoNascimento
+const sobrenome = "Martins"
+
+console.log(`${nome} ${sobrenome} faz ${idadeAtual} anos no ano de ${anoAtual}`)
+
+const redesSociais = ["facebook", "youtube", "instagram", "linkedin"]
