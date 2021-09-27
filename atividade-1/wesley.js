434dd31f4b789fbc5d10fb115025df8b3c8d34a5
WesleyOliveira01 <WesleyOliveira01>
2021-09-27 20:14:20 -0300

    arquivo excluido

--- atividade-1/wesley.js
+++ atividade-1/wesley.js
@@ -1,15 +1,2 @@
-const nome = 'Wesley'
 
-const sobrenome = 'Oliveira Alves'
-
-const ano = 2001
-
-const anoAtual = 2021
-
-const idade = anoAtual - ano
-
-const redes = ['facebook','instagram','linkedin','github','youtube']
-
-
-console.log(`${nome} ${sobrenome} faz ${idade} anos no ano ${anoAtual}`)
 
