60240715d8f7c06608a0e083328abc6f1e946fd6
joaopsilva21 <joaonascimentosilva12@gmail.com>
2021-09-27 21:32:48 -0300

    atividade 27.09

--- atividade-1/joao.js
+++ atividade-1/joao.js
@@ -6,3 +6,40 @@ const sobrenome = "Silva"
 
 console.log (`${nome} ${sobrenome} faz ${idadeAtual} anos no ano de ${AnoAtual}`)
 const redeSocial = ["facebook, instagram, Youtube"]
+
+//Definição da função que verifica idade
+function verificaSeMaiorQue(idadeParametro) {
+    if (idadeAtual >= idadeParametro) {
+        return true 
+    }
+    else{
+        return false
+    }
+}
+
+//Execultar a função que verifica a idade
+const ehMaiorQueDezoito = verificaSeMaiorQue(18)
+console.log(`${nome} é maior que 18? ${ehMaiorQueDezoito}`)
+
+const ehMaiorQueCinquenta = verificaSeMaiorQue(50)
+console.log(`${nome} é maior que 50? ${ehMaiorQueCinquenta}`)
+
+//Definição da função que compara idade
+function comparaIdade ( idadeParametro) {
+    if (idadeAtual > idadeParametro){
+        return "maior"
+    } 
+    else if (idadeAtual < idadeParametro) {
+        return "menor"
+    }
+    else {
+        return "igual"
+    }
+
+//Executar a função que compara a idade
+console.log(`A idade de ${nome} é ${comparaIdade} que 17 anos`)
+console.log(`A idade de ${nome} é ${comparaIdade} que 20 anos`)
+console.log(`A idade de ${nome} é ${comparaIdade} que 23 anos`)
+
+
+}
