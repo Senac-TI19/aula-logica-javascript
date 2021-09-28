dca881bdb5c9d9907b058e612789418abadb5cbe
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-09-27 21:36:27 -0300

    Incremento da Atividade 1

--- atividade-1/marcus.js
+++ atividade-1/marcus.js
@@ -10,3 +10,35 @@ ${idadeAtual} anos no ano de ${anoAtual}`)
 
 const redesSociais = ["facebook", "youtube",
 "instagram"];
+
+//definição da função que verifica a idade
+function verificaSeMaiorQue(idadeParametro){
+   if (idadeAtual >= idadeParametro){
+       return true;
+   } else {
+       return false;
+   }
+}
+
+//executar a função que verifica a idade 
+const ehMaiorQueDezoito = verificaSeMaiorQue(18)
+console.log(`${primeiroNome} é maior que 18? ${ehMaiorQueDezoito}`)
+
+const ehMaiorQueCinquenta = verificaSeMaiorQue(50)
+console.log(`${primeiroNome} é maior que 50? ${ehMaiorQueCinquenta}`)
+
+//Definição da função que compara a idade
+function comparaIdade(idadeParametro){
+    if (idadeAtual > idadeParametro){
+        return "maior"
+    }else if (idadeAtual < idadeParametro){
+        return "menor"
+    } else {
+        return "igual"
+    }
+}
+
+//Executar a função que compara idade
+console.log(`A idade de ${primeiroNome} é ${comparaIdade(20)} que 20 anos`)
+console.log(`A idade de ${primeiroNome} é ${comparaIdade(25)} que 25 anos`)
+console.log(`A idade de ${primeiroNome} é ${comparaIdade(30)} que 30 anos`)
