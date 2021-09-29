3969a076bf5c5043f4865bc6b2ed63bb9ab3eed8
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-09-29 20:00:12 -0300

    Adicionada função

--- atividade-1/marcus.js
+++ atividade-1/marcus.js
@@ -9,15 +9,15 @@ console.log(`${primeiroNome} ${sobrenome} faz
 ${idadeAtual} anos no ano de ${anoAtual}`)
 
 const redesSociais = ["facebook", "youtube",
-"instagram"];
+    "instagram", "linkedin"];
 
 //definição da função que verifica a idade
-function verificaSeMaiorQue(idadeParametro){
-   if (idadeAtual >= idadeParametro){
-       return true;
-   } else {
-       return false;
-   }
+function verificaSeMaiorQue(idadeParametro) {
+    if (idadeAtual >= idadeParametro) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //executar a função que verifica a idade 
@@ -28,10 +28,10 @@ const ehMaiorQueCinquenta = verificaSeMaiorQue(50)
 console.log(`${primeiroNome} é maior que 50? ${ehMaiorQueCinquenta}`)
 
 //Definição da função que compara a idade
-function comparaIdade(idadeParametro){
-    if (idadeAtual > idadeParametro){
+function comparaIdade(idadeParametro) {
+    if (idadeAtual > idadeParametro) {
         return "maior"
-    }else if (idadeAtual < idadeParametro){
+    } else if (idadeAtual < idadeParametro) {
         return "menor"
     } else {
         return "igual"
@@ -42,3 +42,22 @@ function comparaIdade(idadeParametro){
 console.log(`A idade de ${primeiroNome} é ${comparaIdade(20)} que 20 anos`)
 console.log(`A idade de ${primeiroNome} é ${comparaIdade(25)} que 25 anos`)
 console.log(`A idade de ${primeiroNome} é ${comparaIdade(30)} que 30 anos`)
+
+//Declaração da função que verifica linkedin
+function verificaSeTemLinkedin() {
+    if (redesSociais.length === 0) {
+        return false;
+    }
+
+    for (let posicao = 0; posicao < redesSociais.length;
+        posicao = posicao + 1) {
+
+        if (redesSociais[posicao] === "linkedin") {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(`Tem linkedin? ${verificaSeTemLinkedin()}`);
