604ba7f80c9de9495a49801a706b893cd0675ac8
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-10-01 20:13:51 -0300

    Adicionado exibeMensagens antes e depois

---
+++ atividade-2/marcus.js
@@ -0,0 +1,13 @@
+console.log("antes da definição => exibeMensagens")
+
+function exibeMensagens(mensagem1, mensagem2, mensagem3) {
+    console.log(mensagem1)
+    console.log(mensagem2)
+    console.log(mensagem3)
+}
+
+console.log("depois da definição => exibeMensagens")
+
+exibeMensagens("m1", "m2", "m3")
+
+console.log("depois da execução => exibeMensagens")
