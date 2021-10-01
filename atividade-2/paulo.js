dab9e64c4d97d6d826f3eef0b51ab6df2e43cb3d
paulo9110 <paulolopes172@gmail.com>
2021-10-01 20:00:57 -0300

    criando atividade 2

---
+++ atividade-2/paulo.js
@@ -0,0 +1,12 @@
+console.log("Antes - exibeMensagens")
+function exibeMensagens(mensagem1, mensagem2, mensagem3) {
+    console.log(mensagem1) 
+    console.log(mensagem2) 
+    console.log(mensagem3)          
+}
+
+console.log("Depois da definição => exibeMensagens")
+
+exibeMensagens("m1", "m2", "m3", "m4")
+
+console.log("Depois da execução => exibeMensagens")
