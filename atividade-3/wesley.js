4f373719652c17195262f44d90c9e4ff3833bd3f
WesleyOliveira01 <wesleyoliveira.dev@outlook.com>
2021-10-01 21:28:32 -0300

    while

---
+++ atividade-3/wesley.js
@@ -0,0 +1,16 @@
+console.log('m1')
+console.log('m1')
+console.log('m1','m2')
+
+let execucoes = 0
+
+console.log("quantidade de execuções =", execucoes)
+
+console.log('final do codigo')
+
+while(execucoes < 5){
+    execucoes = execucoes + 1
+    console.log("quantidade de execuções =", execucoes)
+    
+}
+console.log('final do codigo')
