2ad7a7da052452017f57266563505f62317ef37e
natiherrera <natiquipildor@gmail.com>
2021-10-04 19:25:56 -0300

    adicionando atividade3

---
+++ atividade-2/natalia.js
@@ -0,0 +1,12 @@
+console.log("antes da definição =>exibeMensagens")
+
+function exibeMensagens(mensagem1, mensagem2) {
+    console.log(mensagem1)
+    console.log(mensagem2)
+    console.log(mensagem3)
+}
+
+console.log("depois da definição => exibeMensagens")
+exibeMensagens("m1", "m2", "m3", "m4")
+
+console.log("depois da execução => exibeMensagens")
