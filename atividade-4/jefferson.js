1a446a1d06631c881eb6236126bde62fb3198961
MarcusVinicius96 <marcus.araujo1996@hotmail.com>
2021-10-05 21:17:55 -0300

    Adicionado for

---
+++ atividade-4/jefferson.js
@@ -0,0 +1,4 @@
+for(let variavelControle = 0;variavelControle < 5; variavelControle = variavelControle + 1){
+
+    console.log(variavelControle)
+}
---
+++ atividade-4/marcus.js
@@ -0,0 +1,25 @@
+//for(let variavelControle = 0;variavelControle < 5; variavelControle = variavelControle + 1){
+//
+//  console.log(variavelControle)
+//}
+
+let totalDeExecucoes = 0
+
+function repeteExecucoes(quantidade) {
+
+    for (let execucoes = 0; execucoes < quantidade; execucoes = execucoes + 1) {
+
+        console.log("Execução =", execucoes)
+    }
+
+    totalDeExecucoes = totalDeExecucoes + execucoes
+    console.log("---------------")
+
+}
+
+repeteExecucoes(3)
+repeteExecucoes(5)
+repeteExecucoes(20)
+repeteExecucoes(10)
+
+console.log("Final do código", totalDeExecucoes)
