a7c2f8df8dd27e3d186b397fe20a61a448786c6d
maxsuelcampos <90655577+maxsuelcampos@users.noreply.github.com>
2021-10-06 16:24:59 -0300

    Update maxsuel.js

--- atividade-3/maxsuel.js
+++ atividade-3/maxsuel.js
@@ -1,9 +1,33 @@
 
 let execucoes = 0
 
-while(execucoes < 5){
-    execucoes = execucoes + 1
-    console.log("quantidade de execuções =", execucoes)
-    
+function repeteExecucoes(quantidade){
+    while(execucoes < quantidade){
+        console.log("quantidade de execuções =", execucoes)
+        execucoes = execucoes + 1
+        
+    }
 }
+
+repeteExecucoes(10)
 console.log('final do codigo')
+
+let totalDeExecucoes = 0
+
+function repeteexecucoes(quantidade){
+    let execucoes = 0
+    while(execucoes < quantidade){
+        execucoes = execucoes + 1
+        console.log("quantidade de execuções =", execucoes) 
+        
+    }
+    totalExecucoes = totalDeExecucoes + execucoes
+    console.log('---------------------')
+}
+
+repeteexecucoes(5)
+repeteexecucoes(10)
+repeteexecucoes(15)
+
+
+console.log('total de execuções',totalExecucoes)
