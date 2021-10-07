b79a9c6d8590d3f6f61250be98b17fd9306baf4d
Valmirfnogueira <valmirfernandesnogueira@gmail.com>
2021-10-06 21:23:43 -0300

    alteração

---
+++ atividade-5/valmir.js
@@ -0,0 +1,52 @@
+const noticia = {
+    titulo: "titulo da noticia",
+    data: "06/10/2021"
+}
+
+console.log(noticia)
+console.log(noticia.titulo)
+console.log(noticia.data)
+
+console.log(noticia.titulo, "publicada em", noticia.data)
+
+const noticia1 = {
+    titulo: "COMBUSTÍVEL",
+    data: "06/10/2021",
+    autor: "josé",
+    imagem:"https://www.webposto.com.br/wp-content/uploads/2021/03/BLOG-GASOSA.png",
+    descricao:"O valor do combustível nas nossas refinarias e terminais é muito inferior ao pago pelo consumidor final.Somente uma parte do preço é da Petrobras"
+
+}
+console.log(noticia1.titulo, "publicada em", noticia1.data, "autor:", noticia1.autor)
+console.log(noticia1.imagem)
+console.log(noticia1.descricao)
+
+const noticia2 = {
+    titulo: "COMBUSTÍVEL",
+    data: "06/10/2021",
+    autor: "josé",
+    imagem:"https://www.webposto.com.br/wp-content/uploads/2021/03/BLOG-GASOSA.png",
+    descricao:"O valor do combustível nas nossas refinarias e terminais é muito inferior ao pago pelo consumidor final.Somente uma parte do preço é da Petrobras"
+
+}
+console.log(noticia1.titulo, "publicada em", noticia1.data, "autor:", noticia1.autor)
+console.log(noticia1.imagem)
+console.log(noticia1.descricao)
+
+const noticia3 = {
+    titulo: "COMBUSTÍVEL",
+    data: "06/10/2021",
+    autor: "josé",
+    imagem:"https://www.webposto.com.br/wp-content/uploads/2021/03/BLOG-GASOSA.png",
+    descricao:"O valor do combustível nas nossas refinarias e terminais é muito inferior ao pago pelo consumidor final.Somente uma parte do preço é da Petrobras"
+
+}
+console.log(noticia1.titulo, "publicada em", noticia1.data, "autor:", noticia1.autor)
+console.log(noticia1.imagem)
+console.log(noticia1.descricao)
+
+function ImprimeNoticiais(){
+
+}
+ImprimeNoticiais()
+console.log(noticia1.titulo, noticia2.titulo, noticia3.titulo)
