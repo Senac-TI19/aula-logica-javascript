5377ac3cf1a501c52ccb93e2221a70cbbca317fc
Valmirfnogueira <valmirfernandesnogueira@gmail.com>
2021-10-14 19:23:44 -0300

    alterado

--- atividade-6/valmir.js
+++ atividade-6/valmir.js
@@ -6,7 +6,7 @@ console.log(noticias.length)
 
 function ImprimeAutores(noticias) {
     let publicacao = 0
-    while (publicacao < 20) {
+    while (publicacao < noticias.length) {
         
          console.log(`${noticias[publicacao].author}`) 
         publicacao = publicacao + 1
@@ -17,8 +17,9 @@ function ImprimeAutores(noticias) {
 
  console.log("-------------------")
 
- for (let publicacao1 = 0; publicacao1 < 20; publicacao1 = publicacao1 + 1) {
+ for (let publicacao1 = 0; publicacao1 < noticias.length; publicacao1 = publicacao1 + 1) {
     console.log(`${noticias[publicacao1].author}`)
 
  }
- 
+
+ console.log("-------------------")
