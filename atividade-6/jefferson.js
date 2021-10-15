5df899f8c2de220934f8329b67dc48ade55643d4
Jefferson Martins <jeffersonmandrade@gmail.com>
2021-10-14 21:06:36 -0300

    inclusão atividade 6

--- atividade-6/jefferson.js
+++ atividade-6/jefferson.js
@@ -1,3 +1,38 @@
-const noticias = require('./noticias.json')
+const noticias = require("./noticias.json");
 
-console.log(noticias)
+// Todos os authores
+function imprimirTodosOsAutores() {
+  for (let posicao = 0; posicao < noticias.length; posicao += 1) {
+    const noticia = noticias[posicao];
+    console.log(noticia.author);
+  }
+}
+
+// Todos os authores únicos e sem os que forem null
+function imprimirTodosOsAutoresUnicos() {
+  const listaAutoresJaImprimidos = []
+
+  for (let posicao = 0; posicao < noticias.length; posicao += 1) {
+    const noticia = noticias[posicao];
+    if (noticia.author !== null) {
+
+      if (!listaAutoresJaImprimidos.includes(noticia.author)) {
+
+        console.log(noticia.author);
+        listaAutoresJaImprimidos.push(noticia.author);
+      }
+    }
+  }
+}
+
+// se author !== null => {title} foi publicada por {author}
+// "Chico Buarque passará por cirurgia no feriado" foi publicada por "Cleo Guimarães"
+// se author === null => {title} foi publicada no {source.name}
+// "Coreia do Sul se prepara para alta de casos de covid-19 durante feriado" foi publicada no "Globo"
+function imprimirDetalhesDaNoticia() {
+
+}
+
+imprimirTodosOsAutores();
+imprimirTodosOsAutoresUnicos();
+imprimirDetalhesDaNoticia();
