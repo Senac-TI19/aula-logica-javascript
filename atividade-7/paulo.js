c53a03225aa55866f6142d58ea808ff0d645f8b4
paulo9110 <paulolopes172@gmail.com>
2021-10-18 19:11:19 -0300

    adicionando atividade 7

---
+++ atividade-7/paulo.js
@@ -0,0 +1,21 @@
+//Callbacks
+//Promise
+
+function executoraDeFuncoes(funcao, quantidadeExecucoes){      
+
+    console.log(`vai executar ${quantidadeExecucoes} vezes`)
+    
+    for(let execucoes = 0; execucoes < quantidadeExecucoes; execucoes = execucoes + 1){
+
+        funcao();
+        
+    }    
+}
+
+executoraDeFuncoes(function (){
+    console.log("primeira chamada");
+}, 2);
+
+executoraDeFuncoes(function (){
+    console.log("segunda chamada");
+}, 4);
