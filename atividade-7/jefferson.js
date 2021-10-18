7154c29e873acdeb87b0d0edd0801cfeb7e65e23
Jefferson Martins <jeffersonmandrade@gmail.com>
2021-10-18 19:37:54 -0300

    conclusão da atividade 7

--- atividade-7/jefferson.js
+++ atividade-7/jefferson.js
@@ -1,2 +1,20 @@
 // Callbacks
-// Promise
+
+function executoraDeFuncoes(funcao, quantidadeExecucoes) {
+  console.log(`vai executar ${quantidadeExecucoes} vezes`);
+  for (let execucoes = 0; execucoes < quantidadeExecucoes; execucoes++) {
+    funcao();
+  }
+}
+
+executoraDeFuncoes(function () {
+  console.log("primeira chamada");
+}, 2);
+
+executoraDeFuncoes(function () {
+  console.log("segunda chamada");
+}, 4);
+
+executoraDeFuncoes(function () {
+  console.log("terceira chamada");
+}, 10);
