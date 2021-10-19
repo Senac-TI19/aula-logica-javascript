47b0b46e85fe04fd529fc38827a36b16220dacf5
Matheus <matheus.malmeida7@gmail.com>
2021-10-18 21:41:04 -0300

    add atividade8

---
+++ atividade-8/matheus.js
@@ -0,0 +1,25 @@
+//promisse
+
+function chamarApi(url, tempo){
+    return new Promise(function (resolve){
+        console.log(`chamando API = ${url}`);
+
+        setTimeout(function() {
+            const resultado = {id:61239, nome: "Fulano"};
+            resolve(resultado);
+        }, tempo *1000);
+    });
+}
+
+const promessa1 = chamarApi("https://api-1.com",10);
+promessa1.then(function (resultadoApi1){
+    console.log(`resultado Api 1 = ${resultadoApi1.id}`);
+});
+
+chamarApi("https://api-2.com",2).then(function (resultadoApi2){
+    console.log(`resultado Api 2 = ${resultadoApi2.id}`);
+});
+
+chamarApi("https://api-3.com",2).then(function (resultadoApi3){
+    console.log(`resultado Api 3 = ${resultadoApi3.id}`);
+});
